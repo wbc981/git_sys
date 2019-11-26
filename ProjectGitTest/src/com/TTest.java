799d72358a712353400e3258a3924f01665431b9
Administrator <bc_wen949@163.com>
2019-11-26 21:08:18 +0800

    第一次提交test

---
+++ ProjectGitTest/src/com/TTest.java
@@ -0,0 +1,10 @@
+package com;
+
+public class TTest {
+    public static void main(String[] args) {
+        System.out.println("13212313");
+        System.out.println("13212313");
+        System.out.println("13212313");
+        System.out.println("13212313");
+    }
+}
